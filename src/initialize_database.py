be5cb47d8e0a63027fd8d3631a9a2fd25406546c
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-04-12 20:07:08 +0300

    Update SQL queries

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -4,14 +4,16 @@ from database_connection import get_database
 def drop_tables(db):
     db.execute("DROP TABLE IF EXISTS Users")
     db.execute("DROP TABLE IF EXISTS Categories")
-    db.execute("DROP TABLE IF EXISTS Expenses")
+    db.execute("DROP TABLE IF EXISTS Transactions")
 
 
 def create_tables(db):
-    db.execute(
-        "CREATE TABLE Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)")
+    db.execute("CREATE TABLE Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)")
     db.execute("CREATE TABLE Categories (id INTEGER PRIMARY KEY, name TEXT)")
-    db.execute("CREATE TABLE Expenses (id INTEGER PRIMARY KEY, date TEXT, amount INTEGER, category_id INTEGER, description TEXT, user_id INTEGER)")
+    db.execute("CREATE TABLE Transactions (id INTEGER PRIMARY KEY, date TEXT, amount INTEGER, category_id INTEGER, description TEXT, user_id INTEGER)")
+
+def insert_categories(db):
+    db.execute("INSERT INTO Categories (name) VALUES ('Ajoneuvot ja liikenne'), ('Asuminen'), ('Harrastukset'), ('Kauneus ja hyvinvointi'), ('Kulttuuri ja viihde'), ('Lapset'), ('Lemmikit'), ('Luoton maksut'), ('Matkailu'), ('Ostokset'), ('Palvelut'), ('Ravintolat ja kahvilat'), ('Ruoka ja päivittäistavarat'), ('Säästöt ja sijoitukset'), ('Terveys'), ('Vaatteet'), ('Vakuutukset'), ('Muut menot'), ('Palkka'), ('Tulonsiirrot'), ('Muut tulot')")
 
 
 def initialize_database():
@@ -19,6 +21,7 @@ def initialize_database():
 
     drop_tables(db)
     create_tables(db)
+    insert_categories(db)
 
 
 if __name__ == "__main__":
