0484a31ab980a29f1529f771e33190e789955c37
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-04-12 16:51:55 +0300

    Add a new Invoke task

--- tasks.py
+++ tasks.py
@@ -15,3 +15,7 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+
+@task()
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
