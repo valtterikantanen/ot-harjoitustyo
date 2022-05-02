feff95f8916bdfb0e4b9433f5ee4c6821d9d8242
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-05-02 19:21:37 +0300

    Improve error handling

--- src/services/budget_service.py
+++ src/services/budget_service.py
@@ -4,6 +4,27 @@ from repositories.user_repository import user_repository
 from repositories.transaction_repository import transaction_repository
 from repositories.category_repository import category_repository
 
+class PasswordsDontMatchError(Exception):
+    pass
+
+class UsernameAlreadyExistsError(Exception):
+    pass
+
+class UserNotFoundError(Exception):
+    pass
+
+class WrongPasswordError(Exception):
+    pass
+
+class InvalidUsernameOrPasswordError(Exception):
+    pass
+
+class AmountInWrongFormatError(Exception):
+    pass
+
+class TooBigNumberError(Exception):
+    pass
+
 
 class BudgetService:
     def __init__(self):
@@ -14,78 +35,52 @@ class BudgetService:
 
     def create_user(self, username, password1, password2):
         if self.user_repository.search_by_username(username):
-            print(f"Käyttäjätunnus {username} on jo käytössä!")
-            return False
+            raise UsernameAlreadyExistsError()
         if password1 != password2:
-            print("Salasanat eivät täsmää!")
-            return False
+            raise PasswordsDontMatchError()
+        if len(username.strip()) == 0 or len(password1.strip()) == 0:
+            raise InvalidUsernameOrPasswordError()
 
         user_id = self.user_repository.create(User(username, password1))
         self.category_repository.add_default_categories(user_id)
-        return True
 
     def login_user(self, username, password):
-        if self.user:
-            print("Olet jo kirjautunut sisään!")
-            return False
-
         result = self.user_repository.search_by_username(username)
         if not result:
-            print(f"Käyttäjää {username} ei löytynyt.")
-            return False
-
+            raise UserNotFoundError()
         if password != result.password:
-            print("Väärä salasana.")
-            return False
+            raise WrongPasswordError()
 
         self.user = result
-        return True
 
     def logout_user(self):
-        if not self.user:
-            print("Et ole kirjautunut sisään.")
-            return False
-
         self.user = None
-        print("Olet kirjautunut ulos.")
-        return True
 
     def add_transaction(self, date, income_or_expense, amount, category, description=None):
-        if not self.user:
-            print("Et ole kirjautunut sisään!")
-            return False
-
         amount = amount.replace(",", ".")
         if income_or_expense == "meno":
-            amount = "-" + amount
+            amount = f"-{amount}"
 
         try:
             amount = int(100 * float(amount))
-        except ValueError:
-            print("Syöttämäsi määrä oli väärässä muodossa.")
-            return False
+        except ValueError as exc:
+            raise AmountInWrongFormatError() from exc
+
+        if amount < -9223372036854775808 or amount > 9223372036854775807:
+            raise TooBigNumberError()
 
         category_id = self.get_category_id(category)
 
         self.transaction_repository.add(Transaction(
             date, amount, category_id, description, self.user))
-        return True
 
     def find_transactions(self):
-        if not self.user:
-            print("Et ole kirjautunut sisään!")
-            return False
-
         return self.transaction_repository.find_all(self.user)
 
     def get_category_id(self, name):
         return self.category_repository.get_category_id(name)
 
     def get_categories(self, income_or_expense):
-        if not self.user:
-            print("Et ole kirjautunut sisään!")
-            return False
-
         user_id = self.user_repository.get_user_id(self.user.username)
 
         if income_or_expense.lower() == "meno":
@@ -97,36 +92,16 @@ class BudgetService:
         return False
 
     def get_all_categories(self):
-        if not self.user:
-            print("Et ole kirjautunut sisään!")
-            return False
-
         user_id = self.user_repository.get_user_id(self.user.username)
         return self.category_repository.find_all_categories(user_id)
 
     def delete_category(self, category_id):
-        if not self.user:
-            print("Et ole kirjautunut sisään!")
-            return False
-
-        #if not self.get_category_id(name):
-            #print("Kategoriaa ei löytynyt.")
-            #return False
-
         user_id = self.user_repository.get_user_id(self.user.username)
-
         self.category_repository.delete(category_id, user_id)
-        return True
 
     def add_category(self, name, category_type):
-        if not self.user:
-            print("Et ole kirjautunut sisään!")
-            return False
-
         user_id = self.user_repository.get_user_id(self.user.username)
-
         self.category_repository.add(name, category_type, user_id)
-        return True
 
 
 budget_service = BudgetService()
