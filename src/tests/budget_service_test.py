18d874ab4b157037737746d2dada63f3dc2226ef
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-05-03 04:45:24 +0300

    Rewrite tests

--- src/tests/budget_service_test.py
+++ src/tests/budget_service_test.py
@@ -35,15 +35,15 @@ class TestBudgetService(unittest.TestCase):
 
     def test_add_income_saves_the_amount_in_right_format(self):
         self.budget_service.login_user("antero", "password123")
-        self.budget_service.add_transaction("12.4.2022", "tulo", "253.69", "Tulonsiirrot", "Opintotuki")
+        self.budget_service.add_transaction("12.4.2022", "income", "253.69", "Tulonsiirrot", "Opintotuki")
         transaction = self.budget_service.find_transactions()[0]
-        self.assertEqual(transaction[1], 25369)
+        self.assertEqual(transaction[2], 25369)
 
     def test_add_expense_saves_the_amount_in_right_format(self):
         self.budget_service.login_user("antero", "password123")
-        self.budget_service.add_transaction("12.4.2022", "meno", "648.23", "Asuminen", "Vuokra")
+        self.budget_service.add_transaction("12.4.2022", "expense", "648.23", "Asuminen", "Vuokra")
         transaction = self.budget_service.find_transactions()[0]
-        self.assertEqual(transaction[1], -64823)
+        self.assertEqual(transaction[2], -64823)
 
     def test_add_transaction_with_too_big_amount(self):
         self.budget_service.login_user("antero", "password123")
@@ -55,24 +55,24 @@ class TestBudgetService(unittest.TestCase):
 
     def test_add_category(self):
         self.budget_service.login_user("antero", "password123")
-        self.assertEqual(len(self.budget_service.get_all_categories()), 21)
+        self.assertEqual(len(self.budget_service.get_categories()), 21)
         self.budget_service.add_category("Testi", "Meno")
-        self.assertEqual(len(self.budget_service.get_all_categories()), 22)
+        self.assertEqual(len(self.budget_service.get_categories()), 22)
 
     def test_delete_category(self):
         self.budget_service.login_user("antero", "password123")
-        self.assertEqual(len(self.budget_service.get_all_categories()), 21)
+        self.assertEqual(len(self.budget_service.get_categories()), 21)
         self.budget_service.delete_category(1)
-        self.assertEqual(len(self.budget_service.get_all_categories()), 20)
+        self.assertEqual(len(self.budget_service.get_categories()), 20)
+
+    def test_show_all_categories(self):
+        self.budget_service.login_user("antero", "password123")
+        self.assertEqual(len(self.budget_service.get_categories()), 21)
 
     def test_show_expense_categories(self):
         self.budget_service.login_user("antero", "password123")
-        self.assertEqual(len(self.budget_service.get_categories("meno")), 18)
+        self.assertEqual(len(self.budget_service.get_categories("expense")), 18)
 
     def test_show_income_categories(self):
         self.budget_service.login_user("antero", "password123")
-        self.assertEqual(len(self.budget_service.get_categories("tulo")), 3)
-
-    def test_show_categories_that_are_not_income_or_expense_categories(self):
-        self.budget_service.login_user("antero", "password123")
-        self.assertEqual(self.budget_service.get_categories("muu"), False)
+        self.assertEqual(len(self.budget_service.get_categories("income")), 3)
